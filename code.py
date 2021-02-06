5990a2851e322bc1460096611fbddf5deb4b3889
S-Abhishek <s.abhishek2016@vitstudent.ac.in>
2021-02-06 09:39:57 +0530

    Added code

--- code.py
+++ code.py
@@ -1,1 +1,12 @@
+a = int(input())
 
+total = a
+value = a
+
+for i in range(a, 0, -1):
+    output = ''
+    for j in range(a, i - 1, -1):
+        output += f'{j}'
+
+    output += f'{j}' * (j - 1)
+    print(output)
